7975c223507c6928eacfd8239ee86d423a8d8064
Martin Probst <martin@probst.io>
2023-05-24 10:29:15 +0200

    Add README.md and some code comments.

---
+++ README.md
@@ -0,0 +1,21 @@
+# tabswitch
+
+A simple Chrome extension to switch between the two most recently used tabs,
+with minimal permissions required. There are other extensions with similar
+functionality, but they often require excessive permissions.
+
+## Installation
+
+You can install this by visiting [Switch Tabs in the Chrome Web
+Store](https://chromewebstore.google.com/detail/jpfcacngcfejgnoaobkgoojockhdlphi).
+
+## Permissions
+
+The extension requires Storage permissions to persist the last used tabs across
+browser restarts.
+
+## Configuration
+
+By default, you can toggle between the two most recently used tabs using
+`Ctrl-Q` (on Mac, Windows, and Linux alike). You can choose a different
+keybinding via `More Tools -> Extensions -> (Hotdog Menu) Keybindings`.
--- tab_listener.ts
+++ tab_listener.ts
@@ -1,21 +1,29 @@
 let lastTabIds: chrome.tabs.TabActiveInfo[] = [];
 loadLastTabIds();
 
+/**
+ * loadLastTabIds loads the list of tabs from persistent storage.
+ *
+ * This extension persists the list of tabs across browser restarts, so that
+ * switching works after a Chrome restart.
+ */
 async function loadLastTabIds() {
-  const storage = await chrome.storage.sync.get({'lastTabIds': '[]'});
+  const storage = await chrome.storage.sync.get({ 'lastTabIds': '[]' });
   lastTabIds = JSON.parse(storage['lastTabIds'] ?? '[]');
   console.log('loaded lastTabIds, got', lastTabIds);
   cleanUpTabs();
 }
 
+/** Persists the list of tab IDs to storage. */
 function saveLastTabIds() {
-  chrome.storage.sync.set({'lastTabIds': JSON.stringify(lastTabIds)});
+  chrome.storage.sync.set({ 'lastTabIds': JSON.stringify(lastTabIds) });
   console.log('stored', lastTabIds.length, 'tabs.');
 }
 
+/** tabActivated records that the given tab is now the most recently used tab. */
 function tabActivated(activeInfo: chrome.tabs.TabActiveInfo) {
   console.log('activated tab', activeInfo);
-  if (lastTabIds.length > 0 && lastTabIds[lastTabIds.length-1].tabId == activeInfo.tabId) {
+  if (lastTabIds.length > 0 && lastTabIds[lastTabIds.length - 1].tabId == activeInfo.tabId) {
     console.log('same tab as last');
     return;
   }
@@ -23,14 +31,33 @@ function tabActivated(activeInfo: chrome.tabs.TabActiveInfo) {
   saveLastTabIds();
 }
 
+/**
+ * Focusing windows is handled differently from switching tabs by Chrome
+ * (there's no additional tab activated event); we need to record both
+ * operations so that switching tabs across windows works.
+ */
+async function windowFocused() {
+  const activeTabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (activeTabs.length !== 1) {
+    return;  // e.g. a developer tools window that has no tabs
+  }
+  const activeTab = activeTabs[0];
+  if (!activeTab.id) {
+    return;
+  }
+  tabActivated({ tabId: activeTab.id, windowId: activeTab.windowId })
+}
+
+/** Removes tabs from our list when they are removed (e.g. closed). */
 function tabRemoved(tabId: number) {
   for (let i = lastTabIds.length - 1; i >= 0; i--) {
     if (lastTabIds[i].tabId === tabId) {
-       lastTabIds.splice(i, 1);
+      lastTabIds.splice(i, 1);
     }
   }
 }
 
+/** cleanUpTabs removes stale entries from `lastTabIds`, i.e. tabs that no longer exist. */
 async function cleanUpTabs() {
   let removed = 0;
   for (let i = lastTabIds.length - 1; i >= 0; i--) {
@@ -47,24 +74,18 @@ async function cleanUpTabs() {
   console.log('cleanUpTabs: removed', removed, 'tabs.');
 }
 
-async function windowFocused() {
-  const activeTabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (activeTabs.length !== 1) {
-    return;  // e.g. a developer tools window that has no tabs
-  }
-  const activeTab = activeTabs[0];
-  if (!activeTab.id) {
-    return;
-  }
-  tabActivated({tabId: activeTab.id, windowId: activeTab.windowId})
-}
-
+/**
+ * activatePreviousTab switches to the previously used tab, it's the main
+ * action of this extension.
+ */
 async function activatePreviousTab() {
   console.log('activate previous tab triggered. Have', lastTabIds.length, 'tabs');
-  if (lastTabIds.length <= 1) { 
+  if (lastTabIds.length <= 1) {
     console.log('no previous tabs');
     return;
   }
+  // Handle the case where a previous tab no longer exists by attempting to
+  // switch to tabs in order, existing on first success.
   for (let i = lastTabIds.length - 2; i >= 0; i--) {
     const previousTab = lastTabIds[i];
     let tab;
@@ -77,8 +98,10 @@ async function activatePreviousTab() {
     }
     if (tab) {
       console.log('found tab, activating', i, previousTab.tabId);
-      chrome.windows.update(previousTab.windowId, {focused: true});
-      chrome.tabs.update(previousTab.tabId, {active: true});
+      // Also focus the window, in case the tab is in a different (background)
+      // window.
+      chrome.windows.update(previousTab.windowId, { focused: true });
+      chrome.tabs.update(previousTab.tabId, { active: true });
       return;
     }
   }
